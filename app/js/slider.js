0f58e8eb9a04be9dcc29c77164bdddc0545a4137
whsh-clothes <65689329+whsh-clothes@users.noreply.github.com>
2020-05-21 03:03:35 +0300

    home

---
+++ app/js/slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function() { // Ждём загрузки страницы
+						   
+	var slides = $(".slider .slides").children(".slide"); // Получаем массив всех слайдов
+	var width = $(".slider .slides").width(); // Получаем ширину видимой области
+	var i = slides.length; // Получаем количество слайдов
+	var offset = i * width; // Задаем начальное смещение и ширину всех слайдов
+	i--; // уменьшаем кол-во слайдов на один ( для проверки в будущем )
+	$(".slider .slides").css('width',offset); // Задаем блоку со слайдами ширину всех слайдов
+	
+	offset = 0; // Обнуляем смещение, так как показывается начала 1 слайд
+	$("body .slider .next").click(function(){	// Событие клика на кнопку "следующий слайд"
+		if (offset < width * i) {	// Проверяем, дошли ли мы до конца
+			offset += width; // Увеличиваем смещение до следующего слайда
+			$(".slider .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)"); // Смещаем блок со слайдами к следующему
+		}
+	});
+
+
+	$("body .slider .prev").click(function(){	// Событие клика на кнопку "предыдущий слайд"
+		if (offset > 0) { // Проверяем, дошли ли мы до конца
+			offset -= width; // Уменьшаем смещение до предыдущегоо слайда
+			$(".slider .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)"); // Смещаем блок со слайдами к предыдущему
+		}
+	});
+
+});
